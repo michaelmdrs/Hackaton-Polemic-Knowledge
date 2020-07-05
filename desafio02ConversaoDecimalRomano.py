3a4b5cb63982941e7818182816a9f350882cfef8
michaelmdrs <mackelldeveloper@gmail.com>
2020-07-05 18:45:59 -0300

    Resolução do desafio

---
+++ desafio02ConversaoDecimalRomano.py
@@ -0,0 +1,21 @@
+#Desafio2 -  Conversor de números romanos para decimais
+print('Conversor de Números Romanos para Decimais')
+print('I, V, X, L, C, D, M')
+
+romano = input('Digite um número Romano: ')
+
+if romano == 'I':
+    print(f'{romano} = 1')
+elif romano == 'V':
+    print(f'{romano} = 5')
+elif romano == 'X':
+    print(f'{romano} = 10')
+elif romano == 'L':
+    print(f'{romano} = 50')
+elif romano == 'C':
+    print(f'{romano} = 100')
+elif romano == 'D':
+    print(f'{romano} = 500')
+elif romano == 'M':
+    print(f'{romano} = 1000')
+
