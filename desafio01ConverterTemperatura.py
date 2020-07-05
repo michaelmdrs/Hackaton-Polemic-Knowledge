66fd3eaccc8cd2b3c72a5022128bcd740f571f7f
michaelmdrs <mackelldeveloper@gmail.com>
2020-07-05 18:04:28 -0300

    Create desafio01ConverterTemperatura.py
    Resolução do desafio

---
+++ desafio01ConverterTemperatura.py
@@ -0,0 +1,19 @@
+#Conversao de temperaturas
+
+desafio = 'Conversor de Graus Celsius para Farenheint e Vice-versa'
+
+print('{:=^20}\n'.format(desafio))
+
+grausCelsius = float(input('Informe a temperatura em °C: '))
+
+farenheit = (grausCelsius * 1.8000) + 32 #Outra formula e ((9 * grausCelsius) / 5) + 32
+
+print('A temperatura de {:.1f}°C corresponde a {:.1f}°F'.format(grausCelsius, farenheit))
+
+print('-' * 30)
+
+grausFahrenheit = float(input('Informe a temperatura em °F: '))
+
+celsius = (grausFahrenheit - 32) / 1.8000
+
+print('A temperatura de {:.1f} corresponde a {:.1f} °C'.format(grausFahrenheit, celsius))
